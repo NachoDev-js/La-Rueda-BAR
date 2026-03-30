df032513277cbad10c27e73cb0eea9a977d8e6d5
NachoDev-js <nacho190010@gmail.com>
2026-03-30 04:45:03 -0300

    Habilitar CORS en .net

--- BarBackend/Program.cs
+++ BarBackend/Program.cs
@@ -3,11 +3,23 @@ using BarBackend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Configuración de la Base de Datos
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// 2. Definición de la política de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") 
+              .AllowAnyHeader()                     
+              .AllowAnyMethod();                    
+    });
+});
+
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PermitirAngular");
 
 app.UseAuthorization();
 
